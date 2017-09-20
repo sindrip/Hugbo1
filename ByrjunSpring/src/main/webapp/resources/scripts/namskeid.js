c8b2a02459f115c0c379b8e4ede0ce93161ddec7
Sindri Petur Laptop <sindripetur@gmail.com>
2017-09-20 15:39:47 +0000

    /namskeid basic

--- ByrjunSpring/src/main/webapp/resources/scripts/namskeid.js
+++ ByrjunSpring/src/main/webapp/resources/scripts/namskeid.js
@@ -1,7 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('hello');
+    Namskeid.init();
 });
 
-let Page = (() => {
+let Namskeid = (() => {
+    let namskeid = {};
 
+    let getNamskeid = () => {
+      const url = document.location.origin + '/api/namskeid';
+
+      $.ajax({
+        url,
+        type: 'GET',
+        success: (res) => {
+          console.log(res);
+        },
+        error: function() {
+          console.log('ajax error');
+        }
+      });
+    };
+
+    let init = () => {
+        console.log('script loaded')
+        getNamskeid();
+    };
+
+
+
+    return {
+        init,
+    };
 })();
