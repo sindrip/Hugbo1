ac9d05a1cc02c3d0fc7a53fea85dbbc82b5e232c
Ævar <aevaradal@gmail.com>
2017-10-18 21:35:52 +0000

    Skjolun

--- ByrjunSpring/src/main/java/is/hi/byrjun/services/ReviewService.java
+++ ByrjunSpring/src/main/java/is/hi/byrjun/services/ReviewService.java
@@ -5,7 +5,10 @@ import is.hi.byrjun.model.Review;
 import java.util.List;
 
 /**
- * Created by Ketill on 2.10.2017.
+ * @author Ketill Guðmundsson
+ * @date 2. Oktober 2017 HBV501G Hugbúnaðarverkefni 1 Háskóli Íslands
+ *
+ * Þjónusta sem talar við Review Repository
  */
 public interface ReviewService {
     public List<Review> getReviewsForCourse(String longNumber);
